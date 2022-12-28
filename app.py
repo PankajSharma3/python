ba1dbdc130b61a916ca4b8c122fa2261771c92d4
PankajSharma3 <118182606+PankajSharma3@users.noreply.github.com>
2022-12-28 09:49:15 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,22 +1,4 @@
-
-a=int(input("Enter the number:"))
-b=input("Enter the operator:")
-c=int(input("Enter the number:"))
-if (a==45 and b=="*" and c==3):
-    print(555)
-elif (a==56 and b=="+" and c==9):
-    print(77)
-elif (a==56 and b=="/" and c==6):
-    print(4)
-elif b=="+":
-    print(a+c)
-elif b=="-":
-    print(a-c)
-elif b=="*":
-    print(a*c)
-elif b=="/":
-    print(a/c)
-elif b=="**":
-    print(a**c)
-else:
-    print("Error")
+l=["Pankaj",34,42,354,3,5,6,"Sharma"]
+for i in l:
+    if str(i).isnumeric() and i>6:
+        print(i)
